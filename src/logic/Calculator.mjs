7553be6a093169d9b6cbd869b9098e976ef1852f
Yevhenii Bakhmat <yevhenii.bakhmat@nure.ua>
2022-12-05 11:09:48 +0300

    Calculator implementation

--- src/logic/Calculator.mjs
+++ src/logic/Calculator.mjs
@@ -1,3 +1,21 @@
+const operations = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => a / b,
+};
+
 export const calculate = (a, b, operation) => {
-  return 123123;
+  if (!Object.keys(operations).includes(operation)) {
+    throw new Error(`Unknown operation: ${operation}`);
+  }
+
+  const parsedA = parseFloat(a);
+  const parsedB = parseFloat(b);
+
+  if (isNaN(parsedA) || isNaN(parsedB)) {
+    throw new Error(`Invalid number: ${a} or ${b}`);
+  }
+
+  return operations[operation](parsedA, parsedB);
 };
